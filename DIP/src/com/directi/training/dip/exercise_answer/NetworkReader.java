61d813bb830a8a0e80f6260ff153b246b7f73629
Nour El Houda Araar <80988838+nouraraar@users.noreply.github.com>
2022-10-10 14:01:40 +0200

    Adjust syntax

--- DIP/src/com/directi/training/dip/exercise_answer/NetworkReader.java
+++ DIP/src/com/directi/training/dip/exercise_answer/NetworkReader.java
@@ -7,21 +7,21 @@ import java.net.URL;
 
 public class NetworkReader implements ReaderInterface
 {
-    private String _protocol;
-    private String _host;
-    private String _file;
+    private String protocol;
+    private String host;
+    private String file;
 
     public NetworkReader(String protocol, String host, String file)
     {
-        _protocol = protocol;
-        _host = host;
-        _file = file;
+        this.protocol = protocol;
+        this.host = host;
+        this.file = file;
     }
 
     @Override
     public String read() throws IOException
     {
-        URL url = new URL(_protocol, _host, _file);
+        URL url = new URL(protocol, host, file);
         InputStream in = url.openStream();
         InputStreamReader reader = new InputStreamReader(in);
         StringBuilder inputString = new StringBuilder();
